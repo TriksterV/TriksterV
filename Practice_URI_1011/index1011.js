e347b4979f8c03bc98b48da55f81c9820b47c197
Kirill Voronov <flash4272@gmail.com>
2021-07-16 17:50:17 +0600

    Finish_URI_1011

--- Practice_URI_1011/index1011.js
+++ Practice_URI_1011/index1011.js
@@ -1,7 +1,7 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 const pi = parseFloat(3.14159)
-const R = parseInt((lines[0])
+const R = parseInt(lines[0])
 const R3 = Math.pow(R ** 3)
 const vl = (4/3) * pi * R3
 console.log(`VOLUME = ${vl}`)
