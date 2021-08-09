633901f63c0eb6ce79e0318c9178e630546fd8c4
Kirill Voronov <flash4272@gmail.com>
2021-08-09 12:43:16 +0600

    URI_1015

--- URI_Task_1015/index1015.js
+++ URI_Task_1015/index1015.js
@@ -1,13 +1,14 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 const parts1  = lines[0].split(' ')
-const parts2  = lines[1].split(' ')
 const x1  = parseFloat(parts1[0])
-const y1 = parseFloat(parts1[1])
+const y1  = parseFloat(parts1[1])
 
+const parts2  = lines[1].split(' ')
 const x2  = parseFloat(parts2[0])
 const y2 = parseFloat(parts2[1])
 
-console.log(`(${x1}, ${y1}), (${x2}, ${y2})`)
+const distance = Math.hypot (x1 - x2, y1 - y2)
 
+console.log(distance.toFixed(4))
 
